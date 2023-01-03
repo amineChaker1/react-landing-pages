94142de611b121ece60da113399316e825d36590
Chaker Amine <chkramine2003@gmail.com>
2023-01-04 00:18:45 +0100

    structre

--- modern-bank/src/App.jsx
+++ modern-bank/src/App.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
+  GetStarted,
+  Footer,
+  FeedbackCard,
+  CTA,
+  Clients,
+  CardDeal,
+  Button,
+  Business,
+  Billing,
+} from "./components";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
